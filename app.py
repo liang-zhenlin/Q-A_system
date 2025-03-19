072663454d7dc977b11b8e0383c3ff3d291c1fa7
liang <liangzhenlin0409@163.com>
2025-03-19 20:15:58 +0800

    init project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import config
+from exts import db
+from models import UserModel
+from blueprints.auth import bp as auth_bp
+from blueprints.qa import bp as qa_bp
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+db.init_app(app)
+
+app.register_blueprint(qa_bp)
+app.register_blueprint(auth_bp)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
